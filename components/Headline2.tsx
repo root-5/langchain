2bb1fbfc0d36a8fee92aa9d3d847b7d44ae8dbad
水村右近 <mizumura_u@cloudseed.co.jp>
2023-09-14 12:31:42 +0900

    [fix]vercelデプロイ時にエラーになった箇所を修正

--- components/Headline2.tsx
+++ components/Headline2.tsx
@@ -1,4 +1,4 @@
-export function Headline2(props: { children: string; className: string }) {
+export function Headline2(props: { children: string; className?: string }) {
     return (
         <h2 className={'pt-7 pb-2 mb-4 text-3xl border-solid border-b border-lime-500 ' + props.className}>
             {props.children}
