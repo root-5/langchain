fffec7e2fa3cb070060f6ed1d3feeeaeab1c6d3b
水村右近 <mizumura_u@cloudseed.co.jp>
2023-09-15 18:43:09 +0900

    [update]ドキュメント一覧を追加、またそれに伴って文章生成のパスをwrittingに変更

--- app/document/page.tsx
+++ app/document/page.tsx
@@ -1,261 +1,142 @@
 'use client';
 
-import { useState } from 'react';
-import Image from 'next/image';
+import { use, useEffect, useState } from 'react';
+import Link from 'next/link';
 import { Headline2 } from '../../components/Headline2';
 
-// 見出しの最大数
-const maxHeadlineNumber = 10;
+// =====================================================================
+// ==== サイトデータ ====
+const siteData = [
+    {
+        short: 'ja',
+        name: 'JavaScript',
+        url: 'https://developer.mozilla.org/ja/docs/Web/JavaScript',
+    },
+    {
+        short: 'ta',
+        name: 'Tailwindcss',
+        url: 'https://tailwindcss.com/docs/installation',
+    },
+    {
+        short: 're',
+        name: 'React',
+        url: 'https://ja.react.dev/learn',
+    },
+    {
+        short: 'ne',
+        name: 'Nextjs',
+        url: 'https://nextjs.org/docs',
+    },
+    {
+        short: 'ty',
+        name: 'TypeScript',
+        url: 'https://www.typescriptlang.org/docs/',
+    },
+    {
+        short: 'la',
+        name: 'Langchain',
+        url: 'https://js.langchain.com/docs/get_started/introduction',
+    },
+];
 
 export default function Page() {
     //====================================================================
     // ==== ステートの宣言 ====
-    const [title, setTitle] = useState(''); // タイトルを管理
-    const [headlineState, setHeadlineState] = useState({ toggle: true, number: 3 }); // 見出しの有無と見出しの数を管理
-    const [textLength, setTextLength] = useState(100); // 本文文字数を管理
-    const [inputHeadlineText, setInputHeadlineText] = useState(['', '', '', '', '', '', '', '', '', '']); // テキストエリアの内容を管理
-    const [result, setResult] = useState({ resultText: '', resultLength: 0 }); //レスポンスの文字数と内容を管理
-    const [isLoading, setIsLoading] = useState(false); // 表示状態を管理
-    const [isError, setIsError] = useState({ statusBoolean: false, messageText: '' }); // エラー状態の有無とエラーメッセージを管理
+    const [searchInput, setSearchInput] = useState({ hide: false, value: '' });
+    const [siteUrl, setSiteUrl] = useState('');
 
     //====================================================================
-    // ==== ボタンの処理 ====
-    // 章作成ボタンが押されたときの処理
-    async function generateHeadline(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setIsLoading(true);
-
-        // フォームの内容を取得し、サーバーに送信
-        try {
-            const serverResponse = await fetch('../api/document/getHeadline', {
-                method: 'POST',
-                body: JSON.stringify({
-                    title: title,
-                    number: headlineState.number,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            // レスポンスをJSONとしてパース
-            const serverResponseObj = await serverResponse.json();
-
-            // 回答部分のみを抽出
-            const resText = serverResponseObj.result;
-
-            // 回答部分を改行で分割し、半角スペースが含まれる場合は半角スペース以降の文字だけを抽出
-            const resTextArray = resText.split('\n');
-            const headlines = resTextArray.map((text: string) => {
-                if (text.includes(' ')) {
-                    const textArray = text.split(' ');
-                    return textArray[1];
-                } else {
-                    return text;
-                }
-            });
-            // headlines配列の長さがmaxHeadlineNumberより小さい場合は、空文字を追加
-            if (headlines.length < maxHeadlineNumber) {
-                for (let i = headlines.length; i < maxHeadlineNumber; i++) {
-                    headlines.push('');
+    // 検索ボックスの入力があった時の処理
+    const seachInputFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput({ hide: false, value: e.target.value });
+        // 入力された値がtailwindcssだった場合は、iframeにフォーカスを当てる
+        for (let i = 0; i < siteData.length; i++) {
+            if (!e.target.value.indexOf(siteData[i].short)) {
+                setSiteUrl(siteData[i].url);
+                setSearchInput({ hide: true, value: '' });
+                const iframe = document.getElementById('iframe') as HTMLIFrameElement;
+                if (iframe) {
+                    iframe.onload = () => {
+                        iframe.focus();
+                    };
                 }
             }
-            setInputHeadlineText(headlines);
-        } catch (error) {
-            const messageText = (error as Error).toString();
-            setIsError({ statusBoolean: true, messageText: messageText });
         }
-        setIsLoading(false);
-    }
+    };
 
-    // 本文生成ボタンが押されたときの処理
-    async function generateBody(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setIsLoading(true);
+    // ショートカットキーの処理
+    // command + shift + / でinputHeadlineの内容を全て削除した上でフォーカスを当てる
+    useEffect(() => {
+        const inputHeadline = document.getElementById('inputHeadline');
+        inputHeadline?.focus();
+    });
 
-        // レスポンスのテキストと長さを仮保存する変数を宣言
-        let resText = '';
-        let resLength = 0;
-
-        // 見出しの数だけループし、見出しの内容を取得
-        for (let i = 0; i < headlineState.number; i++) {
-            // フォームの内容を取得し、サーバーに送信
-            try {
-                const serverResponse = await fetch('../api/document/getBody', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        title: title, // タイトル
-                        headline: inputHeadlineText[i], // 見出しの内容
-                        length: textLength, // 本文の文字数
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-                // レスポンスをJSONとしてパース
-                const serverResponseJson = await serverResponse.json();
-
-                // レスポンスのテキストと長さをステートに保存
-                const text = serverResponseJson.result;
-                const length = text.length;
-
-                resText += text;
-                resLength += length;
-            } catch (error) {
-                const messageText = (error as Error).toString();
-                setIsError({ statusBoolean: true, messageText: messageText });
-            }
-            setResult({ resultText: resText, resultLength: resLength });
-        }
-        setIsLoading(false);
-    }
-
-    //====================================================================
-    // ==== その他の処理 ====
-    // 結果のテキストをコピーする関数
-    function copyText() {
-        const text = result.resultText;
-        navigator.clipboard.writeText(text);
-    }
-
-    //====================================================================
-    // ==== 見出しリストのパーツを生成 ====
-    // numberを上限として、1,2,3...を格納した配列を作成
-    let headlineNumberArr: number[] = [];
-    for (let i = 0; i < headlineState.number; i++) {
-        headlineNumberArr.push(i + 1);
-    }
-    // 見出しリストを作成
-    const listItems = headlineNumberArr.map((num) => (
-        <div key={num} className="flex w-full mt-4 gap-3 items-center">
-            <p className="text-lg">{num}. </p>
-            <input
-                type="text"
-                name="inputHeadline"
-                placeholder={'見出し' + num}
-                value={inputHeadlineText[num - 1]}
-                required
-                onChange={(e) =>
-                    setInputHeadlineText(
-                        inputHeadlineText.map((text, index) => (index !== num - 1 ? text : e.target.value))
-                    )
-                }
-                className="flex-1 p-2 w-full border border-gray-300 rounded-md dark:text-gray-900"
-            />
-        </div>
+    // サイトデータのshortとnameをまとめて一覧にしたitemsパーツを作成
+    const items = siteData.map((item) => (
+        <span>
+            "{item.short}" -&gt; {item.name},{' '}
+        </span>
     ));
 
     //====================================================================
     // ==== レンダリング ====
     return (
-        <main className="max-w-5xl w-11/12 mx-auto pt-14">
-            <Headline2>文章作成</Headline2>
-
-            {/* 入力エリア */}
-            <div className="mt-8">
-                <p className="text-2xl font-black">入力</p>
-                <form onSubmit={generateHeadline} className="flex flex-col">
-                    <label className="mt-4 font-bold">文章のタイトル</label>
-                    <div className="flex w-full mt-2 gap-3 items-center">
-                        <input
-                            type="text"
-                            name="inputHeadline"
-                            id="inputHeadline"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            placeholder="タイトルを入力"
-                            required
-                            className="flex-1 p-2 w-full border border-gray-300 rounded-md dark:text-gray-900"
-                        />
-                    </div>
-                    <div className="flex mt-4 gap-5 items-center">
-                        <label className="font-bold">作成したい章数</label>
-                        <select
-                            name="textLength"
-                            id="textLength"
-                            className="p-2 w-20 border border-gray-300 rounded-md font-normal dark:text-gray-900"
-                            onChange={(e) =>
-                                setHeadlineState({ toggle: headlineState.toggle, number: parseInt(e.target.value) })
-                            }
-                        >
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
-                        </select>
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={isLoading === true}
-                        className="relative mt-4 py-2 px-4 w-28 bg-blue-500 text-white rounded-md duration-300 hover:bg-blue-600 disabled:bg-blue-400 disabled:animate-pulse"
-                    >
-                        見出し生成
-                    </button>
-                </form>
-                <form onSubmit={generateBody} className="mt-10">
-                    <label className="font-bold">見出し構成</label>
-                    <ol className="">
-                        <ul>{listItems}</ul>
-                    </ol>
-                    <div className="flex mt-4 gap-5 items-center">
-                        <label className="font-bold">見出し毎の文字数</label>
-                        <select
-                            name="textLength"
-                            id="textLength"
-                            className="p-2 w-20 border border-gray-300 rounded-md dark:text-gray-900"
-                            onChange={(e) => setTextLength(parseInt(e.target.value))}
-                        >
-                            <option value="100">100</option>
-                            <option value="250">250</option>
-                            <option value="500">500</option>
-                            <option value="750">750</option>
-                            <option value="1000">1000</option>
-                        </select>
-                    </div>
-                    <div className="flex mt-4 gap-5 items-center">
-                        <button
-                            disabled={isLoading === true}
-                            className="relative py-2 px-4 w-28 bg-blue-500 text-white rounded-md duration-300 hover:bg-blue-600 disabled:bg-blue-400 disabled:animate-pulse"
-                        >
-                            本文生成
-                        </button>
-                        <p className="text-red-800 dark:text-white">
-                            生成文の長さ次第ですが、
-                            <br className="sm:hidden" />
-                            かなり時間がかかります！！
-                        </p>
-                    </div>
-                </form>
-                <div className="flex w-fit m-0 justify-center" aria-label="読み込み中"></div>
+        // ifremeを使ってbbcのサイトを表示する
+        <main className="">
+            <div
+                className={
+                    searchInput.hide
+                        ? 'flex gap-2 items-center justify-center w-full absolute bottom-0 left-0 opacity-10 hover:opacity-100'
+                        : 'flex gap-2 items-center justify-center mt-96 mx-auto flex-wrap'
+                }
+            >
+                <div className="block w-full text-center">{items}</div>
+                <Link
+                    href="/"
+                    className="block w-fit m-0 py-2 px-4 bg-blue-500 text-white rounded-md duration-300 hover:before:hidden hover:bg-blue-600"
+                >
+                    /
+                </Link>
+                <Link
+                    href="/cording"
+                    className="block w-fit m-0 py-2 px-4 bg-blue-500 text-white rounded-md duration-300 hover:before:hidden hover:bg-blue-600"
+                >
+                    /cording
+                </Link>
+                <input
+                    type="text"
+                    name="inputHeadline"
+                    id="inputHeadline"
+                    value={searchInput.value}
+                    onChange={seachInputFunc}
+                    placeholder="ライブラリ名を正式名称、英小文字で入力"
+                    required
+                    className={
+                        searchInput.hide
+                            ? 'block m-0 p-2 border border-gray-300 rounded-md dark:text-gray-900 w-full flex-1'
+                            : 'block m-0 p-2 border border-gray-300 rounded-md dark:text-gray-900 w-96'
+                    }
+                />
             </div>
+            {/* <Headline2>テスト</Headline2> */}
+            <iframe
+                id="iframe"
+                src={siteUrl}
+                width="100%"
+                // height="100%"
+                className="focus:border-2 focus:border-yellow-300 h-screen"
+            ></iframe>
+            {/* <iframe src="https://ja.react.dev/learn" width="100%" height="1000px" className="mt-16"></iframe> */}
 
-            {/* 出力エリア */}
-            <div className="mt-14">
-                <p className="text-2xl font-black">出力</p>
-                <p hidden={isError.statusBoolean} className="mt-2 text-gray-700">
-                    {isError.statusBoolean ? isError.messageText : ''}
-                </p>
-                <div className="relative mt-2">
-                    <textarea
-                        className="p-2 h-64 w-full border border-gray-300 rounded-md overflow-y-scroll whitespace-pre-wrap dark:text-gray-900"
-                        placeholder="ここに生成した文章が表示されます"
-                        value={result.resultText}
-                        onChange={(e) => setResult({ resultText: e.target.value, resultLength: result.resultLength })}
-                    ></textarea>
-                    <Image
-                        src="/img/copy.png"
-                        width={24}
-                        height={24}
-                        alt={'コピー'}
-                        className="absolute z-10 bottom-2 right-1 p-2 w-10 h-10 opacity-30 duration-300 rounded-2xl hover:opacity-100 cursor-pointer active:bg-blue-500"
-                        onClick={copyText}
-                    />
-                </div>
-                <p className="text-gray-700 text-right dark:text-white">{result.resultLength}文字</p>
-            </div>
+            {/* ヘッダーとフッターを非表示にする */}
+            <style jsx global>{`
+                header {
+                    display: none;
+                }
+                footer {
+                    display: none;
+                }
+            `}</style>
         </main>
     );
 }
--- components/data/pagesData.tsx
+++ components/data/pagesData.tsx
@@ -1,14 +1,20 @@
 export const pagesData = [
     { key: 1, link: 'summary', name: '文章要約', description: '文章を要約します。日本語以外にも対応しています。' },
-    { key: 2, link: 'document', name: '文章生成', description: '見出しを調整し、文章構成に手を加えることができます。' },
+    { key: 2, link: 'writting', name: '文章生成', description: '見出しを調整し、文章構成に手を加えることができます。' },
     {
         key: 3,
         link: 'cording',
         name: 'コーディング補助',
-        description: 'コードの生成、解説、修正、コメントの付与ができます。',
+        description: 'コードの生成、解説、修正、エラーの解説ができます。',
     },
     {
         key: 4,
+        link: 'document',
+        name: 'ドキュメント一覧',
+        description: 'WEB系言語やライブラリのドキュメントを閲覧できます。',
+    },
+    {
+        key: 5,
         link: 'format',
         name: 'データフォーマット',
         description: '任意のデータを、指定したフォーマットに変換します。',
