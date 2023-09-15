2c80c2888799372eab9c642bd7abec714080fb20
水村右近 <mizumura_u@cloudseed.co.jp>
2023-09-15 22:47:20 +0900

    [update]コーディング補助のモード選択を横並びに変更、またZennモードの実装

--- app/cording/page.tsx
+++ app/cording/page.tsx
@@ -40,6 +40,7 @@ const docArr = [
 export default function Page() {
     //====================================================================
     // ==== ステートの宣言 ====
+    const [isZenn, setIsZenn] = useState(false); // Zennモードを管理
     const [language, setLanguage] = useState('JavaScript'); // モードを管理
     const [mode, setMode] = useState('generate'); // モードを管理
     const [formText, setFormText] = useState(''); // フォームのテキストを管理
@@ -95,15 +96,36 @@ export default function Page() {
         </li>
     ));
 
+    // ヘッダーとフッターを非表示にするパーツを生成
+    const zennHeader = (
+        <style jsx global>{`
+            header {
+                display: none;
+            }
+            footer {
+                display: none;
+            }
+        `}</style>
+    );
+
     //====================================================================
     // ==== レンダリング ====
     return (
-        <main className="max-w-5xl w-11/12 mx-auto pt-14">
+        <main className={isZenn ? 'max-w-5xl w-11/12 mx-auto' : 'max-w-5xl w-11/12 mx-auto pt-14'}>
             <Headline2>コーディング補助</Headline2>
 
             {/* 入力フォーム */}
             <form className="mt-8" onSubmit={submitClick}>
-                <p className="text-2xl font-black">入力</p>
+                <div className="flex items-center justify-between">
+                    <p className="text-2xl font-black">入力</p>
+                    <div
+                        onClick={() => setIsZenn(!isZenn)}
+                        className="relative ml-auto py-1 px-2 bg-blue-800 text-white rounded-md duration-300 hover:bg-blue-600 hover:cursor-pointer"
+                    >
+                        Zenn
+                    </div>
+                    {isZenn ? zennHeader : ''}
+                </div>
                 <div className="flex flex-col">
                     <div className="flex mt-4 gap-5 items-center">
                         <label htmlFor="language" className="font-bold">
@@ -121,21 +143,94 @@ export default function Page() {
                         </select>
                     </div>
                     <div className="flex mt-4 gap-5 items-center">
-                        <label htmlFor="mode" className="font-bold">
-                            モード
-                        </label>
-                        <select
-                            name="mode"
-                            id="mode"
-                            onChange={(e) => setMode(e.target.value)}
-                            className="p-2 w-40 border border-gray-300 rounded-md dark:text-gray-900"
-                        >
-                            <option value={'generate'}>コードの生成</option>
-                            <option value={'explain'}>コードの解説</option>
-                            <option value={'fix'}>コードの修正</option>
-                            <option value={'error'}>エラーの解説</option>
-                            <option value={'comment'}>コメントの挿入</option>
-                        </select>
+                        <p className="font-bold">モード</p>
+                        <ul className="flex-wrap items-center w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li className="w-36 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input
+                                        type="radio"
+                                        id="generate"
+                                        name="mode"
+                                        value={'generate'}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                    />
+                                    <label
+                                        htmlFor="generate"
+                                        className="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        コードの生成
+                                    </label>
+                                </div>
+                            </li>
+                            <li className="w-36 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input
+                                        type="radio"
+                                        id="explain"
+                                        name="mode"
+                                        value={'explain'}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                    />
+                                    <label
+                                        htmlFor="explain"
+                                        className="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        コードの解説
+                                    </label>
+                                </div>
+                            </li>
+                            <li className="w-36 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input
+                                        type="radio"
+                                        id="fix"
+                                        name="mode"
+                                        value={'fix'}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                    />
+                                    <label
+                                        htmlFor="fix"
+                                        className="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        コードの修正
+                                    </label>
+                                </div>
+                            </li>
+                            <li className="w-36 dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input
+                                        type="radio"
+                                        id="error"
+                                        name="mode"
+                                        value={'error'}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                    />
+                                    <label
+                                        htmlFor="error"
+                                        className="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        エラーの解説
+                                    </label>
+                                </div>
+                            </li>
+                            <li className="w-36 dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input
+                                        type="radio"
+                                        id="comment"
+                                        name="mode"
+                                        value={'comment'}
+                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                    />
+                                    <label
+                                        htmlFor="comment"
+                                        className="w-full py-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        コメントの挿入
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                     <textarea
                         name="inputText"
@@ -143,15 +238,15 @@ export default function Page() {
                         value={formText}
                         placeholder={
                             mode === 'generate'
-                                ? '生成したい処理の内容を入力してください\n\n例: \n1. 今年の1月1日から今日までの日数を取得する\n2. 取得した日数をconsole.logで出力する'
+                                ? '生成したい処理の内容を入力してください\n※テキストエリアをダブルクリックでクリップボードの内容を貼り付け\n\n例: \n1. 今年の1月1日から今日までの日数を取得する\n2. 取得した日数をconsole.logで出力する'
                                 : mode === 'explain'
-                                ? '解説してほしいコードを入力してください\n\n例: \nRange("A1:B2").Value'
+                                ? '解説してほしいコードを入力してください\n※テキストエリアをダブルクリックでクリップボードの内容を貼り付け\n\n例: \nRange("A1:B2").Value'
                                 : mode === 'fix'
-                                ? '修正してほしいコードを入力してください\n\n例: \nRange(A1:B2).Value'
+                                ? '修正してほしいコードを入力してください\n※テキストエリアをダブルクリックでクリップボードの内容を貼り付け\n\n例: \nRange(A1:B2).Value'
                                 : mode === 'error'
-                                ? '解説してほしいエラーを入力してください\n\n例: \nインデックスが有効範囲にありません。：実行時エラー9'
+                                ? '解説してほしいエラーを入力してください\n※テキストエリアをダブルクリックでクリップボードの内容を貼り付け\n\n例: \nインデックスが有効範囲にありません。：実行時エラー9'
                                 : mode === 'comment'
-                                ? 'コメントを挿入してほしいコードを入力してください'
+                                ? 'コメントを挿入してほしいコードを入力してください\n※テキストエリアをダブルクリックでクリップボードの内容を貼り付け'
                                 : ''
                         }
                         required
@@ -203,7 +298,7 @@ export default function Page() {
                 <div className="relative mt-2">
                     <SyntaxHighlighter
                         language={language === 'JavaScript' ? 'javascript' : language === 'Python' ? 'python' : 'vba'}
-                        className="mt-2 h-96 w-full rounded-md"
+                        className={result == '' ? 'mt-2 h-32 w-full rounded-md' : 'mt-2 h-96 w-full rounded-md'}
                     >
                         {result}
                     </SyntaxHighlighter>
