e89de6c710568f97e1499ba85d746c9729de9a0e
root-5 <107744643+root-5@users.noreply.github.com>
2023-11-08 15:36:38 +0900

    [little]chatに横方向のスクロールバーが表示される問題の修正

--- app/(basic)/chat/page.tsx
+++ app/(basic)/chat/page.tsx
@@ -181,7 +181,7 @@ export default function Page() {
                     className={
                         isZenn
                             ? 'flex flex-col gap-2 h-[calc(100vh-100px)] overflow-scroll'
-                            : 'flex flex-col gap-2 h-[calc(100vh-240px)] md:h-[calc(100vh-300px)] overflow-scroll'
+                            : 'flex flex-col gap-2 h-[calc(100vh-240px)] md:h-[calc(100vh-300px)] overflow-y-scroll'
                     }
                 >
                     <button
